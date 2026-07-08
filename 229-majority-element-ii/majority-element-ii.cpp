548aa238e2fc7a6e1f9aeb1db5187f54b1eae94c
Vansh Gaur <vanshgaur1106@gmail.com>
2026-07-09 01:29:21 +0530

    Time: 0 ms (100.00%) | Memory: 23.7 MB (64.58%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,17 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        int n = nums.size();
-        map<int , int> mpp ;
-        int mini = int (n/3) + 1 ;
-        for( int i = 0 ; i<n ; i++){
-            mpp[ nums[i] ]++;
-            if( mpp[nums[i]] == mini ){
-                ans.push_back(nums[i]);
-            }
-            if( ans.size() == 2) break;
+       int n = nums.size();
+       int count1 = 0;
+       int count2 = 0;
+       int el1 = INT_MIN;
+       int el2 = INT_MIN;
+
+       for(int i = 0 ; i<n ; i++){
+        if( count1 == 0 && nums[i] != el2 ){
+            count1 = 1;
+            el1 = nums[i];
         }
-        return ans;
+
+        else if( count2 == 0 && nums[i] != el1 ){
+            count2 = 1;
+            el2 = nums[i];
+        }
+
+        else if( el1 == nums[i] ){
+            count1++;
+        }
+        else if( el2 == nums[i] ){
+            count2++;
+        }
+
+        else {
+            count1--;
+            count2--;
+        }
+       }
+
+       int counta = 0 ;
+       int countb = 0;
+       vector<int> ans;
+       int mini = (int)(n/3) + 1;
+
+       for( int i = 0 ; i<n ; i++){
+           if(el1 == nums[i]) counta++;
+           if(el2 == nums[i]) countb++;
+       }
+
+       if( counta>=mini ) ans.push_back(el1);
+       if( countb>=mini ) ans.push_back(el2);
+
+       return ans;
     }
 };
