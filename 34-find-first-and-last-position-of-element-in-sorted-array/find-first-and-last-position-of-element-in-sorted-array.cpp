e88841f5754c6e1f1563bdee1f07f96f4f999097
Vansh Gaur <vanshgaur1106@gmail.com>
2026-07-15 02:01:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (19.91%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -10,18 +10,19 @@ public:
 
         while(low<=high){
             int mid = (low+high)/2;
-            if(nums[mid]>=target){
-                if(nums[mid]==target){
-                    first = mid ;
-                }
-                    high = mid - 1 ;
-                
-            }
-                else{
-                    low = mid + 1;
-                }
             
-        }
+            if(nums[mid] == target) {
+                first = mid;
+                high = mid - 1 ;
+            }
+            else if( nums[mid] < target ) {
+                low = mid + 1;
+            }
+            else{
+                high = mid - 1;
+            }
+            }
+        
 
         low = 0;
         high = n-1 ;
@@ -29,18 +30,17 @@ public:
          while(low<=high){
             int mid = (low+high)/2;
 
-            if(nums[mid] <= target){
-                if(nums[mid]==target){
-                    last = mid ;
-                }
-                    low = mid + 1 ;
-                
-            }
-                else{
-                  high = mid - 1;
-                }
-            
-        }
+           if( nums[mid] == target ){
+                 last = mid;
+                 low = mid + 1;
+           }
+           else if(nums[mid] > target ){
+                high = mid - 1;
+           }
+           else{
+            low = mid + 1;
+           }
+           } 
 
 
         return { first , last };
