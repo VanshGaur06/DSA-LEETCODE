72cc737c3ae17eaec3efeffa81ec7db88a8d27bd
Vansh Gaur <vanshgaur1106@gmail.com>
2026-07-29 00:27:29 +0530

    Time: 48 ms (85.98%) | Memory: 85.5 MB (91.68%) - LeetSync

--- 575-distribute-candies/distribute-candies.cpp
+++ 575-distribute-candies/distribute-candies.cpp
@@ -3,17 +3,12 @@ public:
     int distributeCandies(vector<int>& candyType) {
         sort(candyType.begin() , candyType.end());
         int n = candyType.size();
-        int m = n/2;
         int count = 1;
         for(int i = 0 ; i<n-1 ; i++){
             if(candyType[i] != candyType[i+1]){
                 count++;
             }
         }
-
-        if(m>=count){
-            return count;
-        }
-        return m;
+        return min(count , n/2);
     }
 };
